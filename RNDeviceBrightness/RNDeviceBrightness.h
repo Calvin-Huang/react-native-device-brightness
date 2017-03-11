1c73b1c97736e2d6772f1dce25eff9b9e1ced8f1
Calvin Huang <calvin.peak@capslock.tw>
2017-03-11 17:28:37 +0800

    Remove iOS project from git and add library project.

--- RNDeviceBrightness/RNDeviceBrightness.h
+++ RNDeviceBrightness/RNDeviceBrightness.h
@@ -5,3 +5,11 @@
 //  Created by Calvin on 3/11/17.
 //  Copyright © 2017 CapsLock. All rights reserved.
 //
+
+#import <Foundation/Foundation.h>
+
+@interface RNDeviceBrightness : NSObject
+
+@property (nonatomic, assign) float brightnessLevel;
+
+@end
--- RNDeviceBrightness/RNDeviceBrightness.m
+++ RNDeviceBrightness/RNDeviceBrightness.m
@@ -5,3 +5,11 @@
 //  Created by Calvin on 3/11/17.
 //  Copyright © 2017 CapsLock. All rights reserved.
 //
+
+#import "RNDeviceBrightness.h"
+
+@implementation RNDeviceBrightness
+
+
+
+@end
