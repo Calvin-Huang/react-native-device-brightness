2a079f76772f8cf46a636161541ff47252cf4f9d
Calvin Huang <calvin.peak@capslock.tw>
2017-03-11 17:03:10 +0800

    Add comment header.

---
+++ RNDeviceBrightness.h
@@ -0,0 +1,7 @@
+//
+//  RNDeviceBrightness.h
+//  RNDeviceBrightness
+//
+//  Created by Calvin on 3/11/17.
+//  Copyright © 2017 CapsLock. All rights reserved.
+//
---
+++ RNDeviceBrightness.m
@@ -0,0 +1,7 @@
+//
+//  RNDeviceBrightness.m
+//  RNDeviceBrightness
+//
+//  Created by Calvin on 3/11/17.
+//  Copyright © 2017 CapsLock. All rights reserved.
+//
