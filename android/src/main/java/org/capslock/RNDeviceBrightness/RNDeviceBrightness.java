96bc85068a70f8467776cfe9524352da7cfe3738
Yuichi Tateno <hotchpotch@gmail.com>
2017-05-08 19:45:44 +0900

    Fix for compiler warning

--- android/src/main/java/org/capslock/RNDeviceBrightness/RNDeviceBrightness.java
+++ android/src/main/java/org/capslock/RNDeviceBrightness/RNDeviceBrightness.java
@@ -28,7 +28,7 @@ public class RNDeviceBrightness implements ReactPackage {
 
   @Override
   public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
-    List<NativeModule> modules = new ArrayList();
+    List<NativeModule> modules = new ArrayList<>();
 
     modules.add(new RNDeviceBrightnessModule(reactContext));
 
