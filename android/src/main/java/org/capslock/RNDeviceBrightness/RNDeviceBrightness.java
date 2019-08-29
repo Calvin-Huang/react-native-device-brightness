a94aa48f40608d071e1ae58599314f89f3c58e54
Yuto Uehara <yuto.uehara@mail.rooxim.com>
2019-08-29 17:01:41 +0700

    update

--- android/src/main/java/org/capslock/RNDeviceBrightness/RNDeviceBrightness.java
+++ android/src/main/java/org/capslock/RNDeviceBrightness/RNDeviceBrightness.java
@@ -17,11 +17,6 @@ import java.util.List;
 public class RNDeviceBrightness implements ReactPackage {
 
   @Override
-  public List<Class<? extends JavaScriptModule>> createJSModules() {
-    return Collections.emptyList();
-  }
-
-  @Override
   public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
     return Collections.emptyList();
   }
